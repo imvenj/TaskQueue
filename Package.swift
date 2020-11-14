09b62caafdbd6b5e934652799be5bfa06cb7e1d0
Venj <ersaclarke@gmail.com>
2020-11-14 13:03:39 +0800

    Update swift package

--- Package.swift
+++ Package.swift
@@ -1,5 +1,20 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
-    name: "TaskQueue"
+    name: "TaskQueue",
+    platforms: [.iOS(.v11), .macOS(.v10_10)],
+    products: [
+        .library(
+            name: "TaskQueue",
+            targets: ["TaskQueue"]),
+    ],
+    targets: [
+        .target(
+            name: "TaskQueue",
+            path: "Sources"
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
 )
